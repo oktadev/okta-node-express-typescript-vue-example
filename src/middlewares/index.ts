8bd9a851d714cd3bf0537b9cff1139d0bbb7bd32
David Neal <david@reverentgeek.com>
2018-10-18 16:44:22 -0400

    removes unneeded modules

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,18 +1,6 @@
 /* eslint-disable camelcase */
-import OktaJwtVerifier from "@okta/jwt-verifier";
 import { ExpressOIDC } from "@okta/oidc-middleware";
-import { Client } from "@okta/okta-sdk-nodejs";
 import session from "express-session";
-
-const verifier = new OktaJwtVerifier( {
-	clientId: process.env.OKTA_CLIENT_ID,
-	issuer: `${ process.env.OKTA_ORG_URL }/oauth2/default`
-} );
-
-const client = new Client( {
-	orgUrl: process.env.OKTA_ORG_URL,
-	token: process.env.OKTA_TOKEN
-} );
 
 const oidc = new ExpressOIDC( {
 	client_id: process.env.OKTA_CLIENT_ID,
@@ -26,9 +14,9 @@ const initializeApp = ( app: any ) => {
 	app.use( session( {
 		resave: true,
 		saveUninitialized: false,
-		secret: process.env.APP_SECRET
+		secret: process.env.SESSION_SECRET
 	} ) );
 	app.use( oidc.router );
 };
 
-export { client, initializeApp, verifier, oidc };
+export { initializeApp, oidc };
